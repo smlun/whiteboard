9dd9dac7d7efc93c1d1c2cbf0f827612474565d2
benja <benjabuddy91@gmail.com>
2017-03-01 14:55:13 +0800

    Generate User model with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,32 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :username,
+            :presence => true,
+            :uniqueness => {
+            :case_sensitive => false
+            } # etc.
+
+  validate :validate_username
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:login]
+
+  attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
 end
