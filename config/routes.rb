89450ad721f61cab51eb57e249b74c07ed2e81c8
benja <benjabuddy91@gmail.com>
2017-03-02 16:39:50 +0800

    Nest comments under topics in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :topics
+  resources :topics do
+    resources :comments, only: [:create, :edit, :update, :destroy]
+  end
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
