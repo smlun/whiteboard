2f081514a5a7d5be2a9c055f004c9a21d6bfdab8
benja <benjabuddy91@gmail.com>
2017-03-02 14:00:35 +0800

    Populate seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+dummy_content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu diam magna. Maecenas et tellus id turpis varius sagittis vitae a erat. Nulla accumsan convallis vulputate. Suspendisse non erat purus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec a lobortis metus. Fusce nec nisl purus. Ut id mi id mi rhoncus dapibus at a urna. Donec ullamcorper congue consequat. Fusce euismod consequat dui, id condimentum nulla pellentesque sed.'
+
+newuser = User.create({username: 'railspractice91', email: 'railspractice91@gmail.com', password: 'password', password_confirmation: 'password'})
+
+topics = Topic.create([{title: "How do we do testing on Rails?", content: dummy_content, user: newuser},
+                       {title: "Where do we start on learning GIT?", content: dummy_content, user: newuser},
+                       {title: "Should we learn Angular.js or React.js?", content: dummy_content, user: newuser},
+                       {title: "Is xecutioner a typo?", content: dummy_content, user: newuser}])
