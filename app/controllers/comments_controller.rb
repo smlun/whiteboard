19ec28e23d513d251afb38559b285e14ccb66148
benja <benjabuddy91@gmail.com>
2017-03-02 16:42:16 +0800

    Implement create/delete function for comment model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,17 +6,21 @@ class CommentsController < ApplicationController
   def create
     @topic = Topic.find(params[:topic_id])
     @comment = Comment.new(comment_params)
+    @comment.ratings = 0
     @comment.topic = @topic
     @comment.user = current_user
-    @comment.save
+    @comment.save!
     redirect_to @topic
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to topic_path(params[:topic_id])
   end
 
 private
   def comment_params
-    params.require(:comment).permit(:content, :ratings)
+    params.require(:comment).permit(:content)
   end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -32,6 +32,7 @@ class TopicsController < ApplicationController
 
   def show
     @topic = Topic.find(params[:id])
+    @comment = Comment.new
   end
 
   private
